e9f7bae6ca25856a355ae5e72fe5d07731057228
Albina Salkayeva <salkaeva.a@gmail.com>
2023-06-18 11:45:15 +0600

    Make share panel for desktop, change share button icons

--- site/script.js
+++ site/script.js
@@ -1,4 +1,5 @@
-const shareBtn = document.getElementById('author-block__share-btn');
+const shareBtn = document.querySelector('.author-block__share-btn');
+const shareBtnImg = shareBtn.querySelector('img');
 const sharePanel = document.querySelector('.share-panel');
 const previewCardContent = document.querySelector('.preview-card__content');
 
@@ -6,8 +7,10 @@ shareBtn.addEventListener('click', () => {
   if (sharePanel.classList.contains('flex')) {
     sharePanel.classList.remove('flex');
     previewCardContent.classList.remove('padding-reduce');
+    shareBtnImg.setAttribute('src', './images/icon-share-1.svg');
   } else {
     sharePanel.classList.add('flex');
     previewCardContent.classList.add('padding-reduce');
+    shareBtnImg.setAttribute('src', './images/icon-share-2.svg');
   }
 });
