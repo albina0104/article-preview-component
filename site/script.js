37ba778ecfd9011f282ac06ae20749899f77f64f
Albina Salkayeva <salkaeva.a@gmail.com>
2023-06-18 18:51:02 +0600

    Make the share panel closed when clicked outside

--- site/script.js
+++ site/script.js
@@ -1,16 +1,19 @@
 const shareBtn = document.querySelector('.author-block__share-btn');
 const shareBtnImg = shareBtn.querySelector('img');
 const sharePanel = document.querySelector('.share-panel');
+const closeSharePanel = document.querySelector('.close-share-panel');
 const previewCardContent = document.querySelector('.preview-card__content');
 
 shareBtn.addEventListener('click', () => {
-  if (sharePanel.classList.contains('flex')) {
-    sharePanel.classList.remove('flex');
-    previewCardContent.classList.remove('padding-reduce');
-    shareBtnImg.setAttribute('src', './images/icon-share-1.svg');
-  } else {
-    sharePanel.classList.add('flex');
-    previewCardContent.classList.add('padding-reduce');
-    shareBtnImg.setAttribute('src', './images/icon-share-2.svg');
-  }
+  sharePanel.classList.add('flex');
+  previewCardContent.classList.add('padding-reduce');
+  shareBtnImg.setAttribute('src', './images/icon-share-2.svg');
+  closeSharePanel.classList.add('block');
+});
+
+closeSharePanel.addEventListener('click', () => {
+  sharePanel.classList.remove('flex');
+  previewCardContent.classList.remove('padding-reduce');
+  shareBtnImg.setAttribute('src', './images/icon-share-1.svg');
+  closeSharePanel.classList.remove('block');
 });
